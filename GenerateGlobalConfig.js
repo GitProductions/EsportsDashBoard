2fc0731040eeb00b4e4490f55fc7139530b0699f
Gitago <76603653+gitagogaming@users.noreply.github.com>
2025-02-01 03:18:43 -0500

    new global config format

--- GenerateGlobalConfig.js
+++ GenerateGlobalConfig.js
@@ -2,56 +2,61 @@ const fs = require('fs');
 const path = require('path');
 
 class StarterPack {
-  constructor({ name, author, version, game, image, description, fileKey, zipFile }) {
+  constructor({ name, author, version, image, description, folderPath, fileName }) {
     this.name = name;
-    this.author = author;
+    this.author = author; 
     this.version = version;
-    this.game = game;
     this.image = image;
     this.description = description;
-    this.fileKey = fileKey;
-    this.zipFile = zipFile;
+    this.folderPath = folderPath;
+    this.fileName = fileName;
   }
 }
 
 class HtmlPack {
-  constructor({ name, author, version, game, image, description, fileKey, zipFile }) {
+  constructor({ name, author, version, image, description, folderPath, fileName }) {
     this.name = name;
     this.author = author;
     this.version = version;
-    this.game = game;
     this.image = image;
     this.description = description;
-    this.fileKey = fileKey;
-    this.zipFile = zipFile;
+    this.folderPath = folderPath;
+    this.fileName = fileName;
   }
 }
 
 class GameConfig {
-  constructor({ id, name, author, version, image, description, fileKey }) {
+  constructor({ id, name, author, version, image, description, folderPath, fileName }) {
     this.id = id;
     this.name = name;
     this.author = author;
     this.version = version;
-    this.image = image;
+    this.image = image; 
     this.description = description;
-    this.fileKey = fileKey;
+    this.folderPath = folderPath;
+    this.fileName = fileName;
   }
 }
 
 class ConfigGenerator {
   constructor() {
-    this.starterPacks = [];
-    this.htmlPacks = [];
+    this.starterPacks = {};
+    this.htmlPacks = {};
     this.gameConfigs = {};
   }
 
-  addStarterPack(pack) {
-    this.starterPacks.push(pack);
+  addStarterPack(game, pack) {
+    if (!this.starterPacks[game]) {
+      this.starterPacks[game] = [];
+    }
+    this.starterPacks[game].push(pack);
   }
 
-  addHtmlPack(pack) {
-    this.htmlPacks.push(pack);
+  addHtmlPack(game, pack) {
+    if (!this.htmlPacks[game]) {
+      this.htmlPacks[game] = [];
+    }
+    this.htmlPacks[game].push(pack);
   }
 
   addGameConfig(game, config) {
@@ -79,165 +84,155 @@ class ConfigGenerator {
 // Create config generator instance
 const configGenerator = new ConfigGenerator();
 
-// Starter Packs
-const starterPacks = [
-
-  {
-    name: "BGG Overwatch Starter Pack",
-    author: "EsportsDash",
-    version: "1.3",
-    game: "Overwatch",
-    image: "Game Configs/Overwatch/Overwatch.webp",
-    description: "Starter Pack includes Game Config, HTML Files & OBS Scene Collection",
-    fileKey: "Starter Packs",
-    zipFile: "BGG_Overwatch_EsportsDash_Starter_v1.3.bggstarter"
-  },
-
-  {
-    name: "BGG Marvel Rivals Starter Pack",
-    author: "EsportsDash",
-    version: "1.1",
-    game: "Marvel Rivals",
-    image: "Game Configs/Marvel Rivals/Marvel Rivals.webp",
-    description: "Starter Pack includes Game Config, HTML Files & OBS Scene Collection",
-    fileKey: "Starter Packs",
-    zipFile: "MarvelRivals_Starter_v1.1.bggstarter"
-  },
+// Define configurations
+const configurations = {
 
 
-  {
-    name: "Valorant Starter Pack",
-    author: "EsportsDash",
-    version: "1.1",
-    game: "Valorant",
-    image: "Game Configs/Valorant/Valorant.webp",
-    description: "Starter Pack includes Game Config, HTML Files & OBS Scene Collection",
-    fileKey: "Starter Packs",
-    zipFile: "Valorant_Starter_v1.1.bggstarter"
-  }
-];
-
-// HTML Packs
-const htmlPacks = [
-  {
-    name: "Broadcast GG Overwatch HTML Pack",
-    author: "Broadcast GG",
-    version: "1.3",
-    game: "Overwatch",
-    image: "Game Configs/Overwatch/Overwatch.webp",
-    description: "Description of HTML pack 1",
-    fileKey: "HTML Packs/Overwatch-BGG",
-    zipFile: "Overwatch-BGG-HTML-PACK.zip"
-  },
-
-
-  {
-    name: "Broadcast GG Valorant HTML Pack",
-    author: "Broadcast GG",
-    version: "1.1",
-    game: "Valorant",
-    image: "Game Configs/Valorant/Valorant.webp",
-    description: "Description of HTML pack 2",
-    fileKey: "HTML Packs/Valorant",
-    zipFile: "Valorant-BGG-HTML-PACK.zip"
-  },
-
-
-  {
-    name: "Broadcast GG Marvel Rivals HTML Pack",
-    author: "Broadcast GG",
-    version: "1.1",
-    game: "Marvel Rivals",
-    image: "Game Configs/Marvel Rivals/Marvel Rivals.webp",
-    description: "Description of HTML pack 3",
-    fileKey: "HTML Packs/Marvel-BGG",
-    zipFile: "MarvelRivals-BGG-HTML-PACK.zip"
-  }
-];
-
-// Game Configs
-const gameConfigs = {
-  "Overwatch": [
-    {
-      id: "esportsdash.overwatch",
-      name: "Overwatch Game Config",
-      author: "EsportsDash",
-      version: "1.2",
-      image: "Game Configs/Overwatch/Overwatch.webp",
-      description: "NOT USING GAME CONFIG SECTION HERE YET.... on website. anyhow..",
-      fileKey: "Game Configs/Overwatch/Overwatch-EsportsDash-1.2.bgg"
-    },
-
-
-    {
-      id: "esportsdash.overwatch",
-      name: "Overwatch Game Config",
+  "Overwatch": {
+    starterPack: {
+      name: "BGG Overwatch Starter Pack",
       author: "EsportsDash",
       version: "1.3",
       image: "Game Configs/Overwatch/Overwatch.webp",
-      description: "NOT USING GAME CONFIG SECTION HERE YET.... on website. anyhow..",
-      fileKey: "Game Configs/Overwatch/esportsdash.overwatch_v1.3"
-    }
-  ],
-
-
-  "Valorant": [
-    {
-      id: "esportsdash.valorant",
-      name: "Valorant Game Config",
-      author: "EsportsDash",
-      version: "1.2",
-      image: "Game Configs/Valorant/valorant.png",
-      description: "Description of game config 2",
-      fileKey: "Game Configs/Valorant/Valorant-EsportsDash-1.2.bgg"
+      description: "Starter Pack includes Game Config, HTML Files & OBS Scene Collection",
+      folderPath: "Starter Packs",
+      fileName: "BGG_Overwatch_EsportsDash_Starter_v1.3.bggstarter"
     },
 
 
-    {
-      id: "esportsdash.valorant",
-      name: "Valorant Game Config",
-      author: "EsportsDash",
+    htmlPack: {
+      name: "Broadcast GG Overwatch HTML Pack",
+      author: "Broadcast GG",
       version: "1.3",
-      image: "Game Configs/Valorant/valorant.png",
-      description: "Description of game config 2",
-      fileKey: "Game Configs/Valorant/esportsdash.valorant_v1.3.bgg"
-    }
-  ],
-
-
-  "Marvel Rivals": [
-    {
-      id: "esportsdash.marvelrivals",
-      name: "Marvel Rivals Game Config",
-      author: "EsportsDash",
-      version: "1.0",
-      image: "Game Configs/Marvel Rivals/Marvel Rivals.webp",
-      description: "Description of game config 3",
-      fileKey: "Game Configs/Marvel Rivals/Marvel Rivals-config.bgg"
+      image: "Game Configs/Overwatch/Overwatch.webp",
+      description: "Description of HTML pack 1",
+      folderPath: "HTML Packs/Overwatch-BGG",
+      fileName: "Overwatch-BGG-HTML-PACK.zip"
     },
 
-    
-    {
-      id: "esportsdash.marvelrivals",
-      name: "Marvel Rivals Game Config",
+
+    gameConfigs: [
+      {
+        id: "esportsdash.overwatch",
+        name: "Overwatch Game Config",
+        author: "EsportsDash",
+        version: "1.2",
+        image: "Game Configs/Overwatch/Overwatch.webp",
+        description: "Game configuration file.",
+        folderPath: "Game Configs/Overwatch",
+        fileName: "Overwatch-EsportsDash-1.2.bgg"
+      },
+      {
+        id: "esportsdash.overwatch",
+        name: "Overwatch Game Config",
+        author: "EsportsDash",
+        version: "1.3",
+        image: "Game Configs/Overwatch/Overwatch.webp",
+        description: "Game configuration file.",
+        folderPath: "Game Configs/Overwatch",
+        fileName: "esportsdash.overwatch_v1.3.bgg"
+      }
+    ]
+  },
+
+
+
+  "Valorant": {
+
+    starterPack: {
+      name: "Valorant Starter Pack",
+      author: "EsportsDash",
+      version: "1.1",
+      image: "Game Configs/Valorant/Valorant.webp",
+      description: "Starter Pack includes Game Config, HTML Files & OBS Scene Collection",
+      folderPath: "Starter Packs",
+      fileName: "Valorant_Starter_v1.1.bggstarter"
+    },
+
+
+    htmlPack: {
+      name: "Broadcast GG Valorant HTML Pack",
+      author: "Broadcast GG",
+      version: "1.1",
+      image: "Game Configs/Valorant/Valorant.webp",
+      description: "Description of HTML pack 2",
+      folderPath: "HTML Packs/Valorant",
+      fileName: "Valorant-BGG-HTML-PACK.zip"
+    },
+
+
+    gameConfigs: [
+      {
+        id: "esportsdash.valorant",
+        name: "Valorant Game Config",
+        author: "EsportsDash",
+        version: "1.2",
+        image: "Game Configs/Valorant/valorant.png",
+        description: "Game configuration file.",
+        folderPath: "Game Configs/Valorant",
+        fileName: "Valorant-EsportsDash-1.2.bgg"
+      },
+      {
+        id: "esportsdash.valorant",
+        name: "Valorant Game Config",
+        author: "EsportsDash",
+        version: "1.3",
+        image: "Game Configs/Valorant/valorant.png",
+        description: "Game configuration file.",
+        folderPath: "Game Configs/Valorant",
+        fileName: "esportsdash.valorant_v1.3.bgg"
+      }
+    ]
+  },
+
+
+
+  "Marvel Rivals": {
+
+    starterPack: {
+      name: "BGG Marvel Rivals Starter Pack",
       author: "EsportsDash",
       version: "1.1",
       image: "Game Configs/Marvel Rivals/Marvel Rivals.webp",
-      description: "Description of game config 3",
-      fileKey: "Game Configs/Marvel Rivals/esportsdash.marvelrivals_v1.1.bgg"
-    }
-  ]
+      description: "Starter Pack includes Game Config, HTML Files & OBS Scene Collection",
+      folderPath: "Starter Packs",
+      fileName: "MarvelRivals_Starter_v1.1.bggstarter"
+    },
+
+
+    htmlPack: {
+      name: "Broadcast GG Marvel Rivals HTML Pack",
+      author: "Broadcast GG",
+      version: "1.1",
+      image: "Game Configs/Marvel Rivals/Marvel Rivals.webp",
+      description: "Description of HTML pack 3",
+      folderPath: "HTML Packs/Marvel-BGG",
+      fileName: "MarvelRivals-BGG-HTML-PACK.zip"
+    },
+
+    
+    gameConfigs: [
+      {
+        id: "esportsdash.marvelrivals",
+        name: "Marvel Rivals Game Config",
+        author: "EsportsDash",
+        version: "1.1",
+        image: "Game Configs/Marvel Rivals/Marvel Rivals.webp",
+        description: "Game configuration file.",
+        folderPath: "Game Configs/Marvel Rivals",
+        fileName: "esportsdash.marvelrivals_v1.1.bgg"
+      }
+    ]
+  }
 };
 
-// Add starter packs
-starterPacks.forEach(pack => configGenerator.addStarterPack(new StarterPack(pack)));
-
-// Add HTML packs
-htmlPacks.forEach(pack => configGenerator.addHtmlPack(new HtmlPack(pack)));
-
-// Add game configs
-Object.entries(gameConfigs).forEach(([game, configs]) => {
-  configs.forEach(config => configGenerator.addGameConfig(game, new GameConfig(config)));
+// Add all configurations
+Object.entries(configurations).forEach(([game, config]) => {
+  configGenerator.addStarterPack(game, new StarterPack(config.starterPack));
+  configGenerator.addHtmlPack(game, new HtmlPack(config.htmlPack));
+  config.gameConfigs.forEach(gameConfig => {
+    configGenerator.addGameConfig(game, new GameConfig(gameConfig));
+  });
 });
 
 // Save to JSON file
